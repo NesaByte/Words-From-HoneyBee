2cf4109f805dd51da292db78343ec5a2f66404e6
Nesa Bee <nesabertanico@yahoo.com.ph>
2020-04-09 19:31:09 -0400

    pls work

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(__dirname + "/dist/final-a2"));
+app.use(express.static(__dirname ));
 
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/final-a2/index.html"));
+  res.sendFile(path.join(__dirname + "/index.html"));
 });
 
 
